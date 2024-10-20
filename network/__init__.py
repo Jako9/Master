e426bba5188ceab760b4358c2d5173e97cf0646a
Jako9 <cubecrafter.lpt@gmail.com>
2024-10-20 23:44:39 +0200

    Added support for SNN architecture in a RL environment

--- network/__init__.py
+++ network/__init__.py
@@ -3,6 +3,7 @@ from .plasticity_injection import Plasticity_Injection
 from .reset_head import Reset_Head
 from .layer_norm import Layer_Norm
 from .ridig import Rigid
+from .rigid_snn import Rigid_SNN
 from .reset_network import Reset_Network
 
-__all__ = ['Plastic', 'Plasticity_Injection', 'Layer_Norm', 'Reset_Head', 'Rigid', 'Reset_Network']
+__all__ = ['Plastic', 'Plasticity_Injection', 'Layer_Norm', 'Reset_Head', 'Rigid', 'Rigid_SNN', 'Reset_Network']
--- network/base_network.py
+++ network/base_network.py
@@ -41,6 +41,7 @@ It provides interfaces for the three common plasticity methods.
 class Large_Network(Plastic):
     def __init__(self, env, *args, **kwargs):
         super().__init__()
+
         self.conv2d_1 = nn.Conv2d(4, 32, 8, stride=4)
         self.conv2d_2 = nn.Conv2d(32, 64, 4, stride=2)
         self.conv2d_3 = nn.Conv2d(64, 64, 3, stride=1)
@@ -66,5 +67,73 @@ class Large_Network(Plastic):
     def _forward(self, x):
         x = self.body(x)
         return self.head(x)
+    
+import snntorch as snn
+from snntorch import spikegen
+import torch
+
+class Large_SNN(Plastic):
+    def __init__(self, env, *args, **kwargs):
+        super().__init__()
+
+        self.num_steps = 10
+
+        self.conv2d_1 = nn.Conv2d(4, 32, 8, stride=4)
+        self.conv2d_2 = nn.Conv2d(32, 64, 4, stride=2)
+        self.conv2d_3 = nn.Conv2d(64, 64, 3, stride=1)
+
+        
+        self.lif1 = snn.Leaky(beta=0.95)
+        self.lif2 = snn.Leaky(beta=0.95)
+        self.lif3 = snn.Leaky(beta=0.95)
+        self.lif_fc = snn.Leaky(beta=0.95)
+        
+        self.flatten = nn.Flatten()
+
+        self.linear = nn.Linear(3136, 512)
+        self.head = nn.Linear(512, env.single_action_space.n)
+
+        self.body = nn.Sequential(
+            self.conv2d_1,
+            self.lif1,
+            self.conv2d_2,
+            self.lif2,
+            self.conv2d_3,
+            self.lif3,
+            self.flatten,
+            self.linear,
+            self.lif_fc
+        )
+
+    def _forward(self, x):
+        
+        spike_train = spikegen.rate(x, num_steps=self.num_steps)
+
+        mem1 = self.lif1.init_leaky()
+        mem2 = self.lif2.init_leaky()
+        mem3 = self.lif3.init_leaky()
+        mem_fc = self.lif_fc.init_leaky()
+
+        spk_out = torch.zeros(x.size(0), 512, device=x.device)
+
+        for step in range(self.num_steps):
+            x = self.conv2d_1(spike_train[step])
+            spk1, mem1 = self.lif1(x, mem1)
+
+            x = self.conv2d_2(spk1)
+            spk2, mem2 = self.lif2(x, mem2)
+
+            x = self.conv2d_3(spk2)
+            spk3, mem3 = self.lif3(x, mem3)
+
+            x = self.flatten(spk3)
+            x = self.linear(x)
+            spk_fc, mem_fc = self.lif_fc(x, mem_fc)
+
+            spk_out += spk_fc
+
+        return self.head(spk_out / self.num_steps)
+
+
     
 
