b00155b32d74941630f3ef5d09ad425b5ba6aecc
Jako9 <cubecrafter.lpt@gmail.com>
2024-10-20 22:10:05 +0200

    Renaming 'No_Plasticity' to 'Rigid'

--- network/__init__.py
+++ network/__init__.py
@@ -2,7 +2,7 @@ from .base_network import Plastic
 from .plasticity_injection import Plasticity_Injection
 from .reset_head import Reset_Head
 from .layer_norm import Layer_Norm
-from .no_plasticity import No_Plasticity
+from .ridig import Rigid
 from .reset_network import Reset_Network
 
-__all__ = ['Plastic', 'Plasticity_Injection', 'Layer_Norm', 'Reset_Head', 'No_Plasticity', 'Reset_Network']
+__all__ = ['Plastic', 'Plasticity_Injection', 'Layer_Norm', 'Reset_Head', 'Rigid', 'Reset_Network']
--- network/ridig.py
+++ network/ridig.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 from .base_network import Large_Network
 
-class No_Plasticity(Large_Network):
+class Rigid(Large_Network):
     def __init__(self, env, *args, **kwargs):
         super().__init__(env)
