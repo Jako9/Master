f32d94bf001e5aebf632517279357753ccce2199
Jako9 <cubecrafter.lpt@gmail.com>
2024-10-25 01:25:10 +0200

    Added Plasticity for every drift to use 2 Layers

--- network/plasticity_injection_drift.py
+++ network/plasticity_injection_drift.py
@@ -10,11 +10,24 @@ class Plasticity_Injection_Drift(Large_Network):
         self.plasticity_bias_correction = nn.Linear(512, env.single_action_space.n)
         self.plasticity_bias_correction.load_state_dict(self.plasticity_bias.state_dict())
 
-        self._change_grad(self.head, False)
+        self.plasticity_bias_linear = nn.Linear(3136, 512)
+
+        self.plasticity_bias_correction_linear = nn.Linear(3136, 512)
+        self.plasticity_bias_correction_linear.load_state_dict(self.plasticity_bias_linear.state_dict())
+
+        self._change_grad(self.plasticity_bias, False)
         self._change_grad(self.plasticity_bias_correction, False)
 
+        self._change_grad(self.plasticity_bias_linear, False)
+        self._change_grad(self.plasticity_bias_correction_linear, False)
+
     def _forward(self, x):
-        x = self.body(x)
+        x = self.relu(self.conv2d_1(x))
+        x = self.relu(self.conv2d_2(x))
+        x = self.relu(self.conv2d_3(x))
+        x = self.flatten(x)
+        x = self.linear(x) + (self.plasticity_bias_linear(x) - self.plasticity_bias_correction_linear(x))
+        x = self.relu(x)
         return self.head(x) + (self.plasticity_bias(x) - self.plasticity_bias_correction(x))
 
     def every_drift(self, num_drift):
@@ -22,9 +35,15 @@ class Plasticity_Injection_Drift(Large_Network):
         self.head.weight = nn.Parameter(self.head.weight + self.plasticity_bias.weight - self.plasticity_bias_correction.weight)
         self.head.bias = nn.Parameter(self.head.bias + self.plasticity_bias.bias - self.plasticity_bias_correction.bias)
 
+        self.linear.weight = nn.Parameter(self.linear.weight + self.plasticity_bias_linear.weight - self.plasticity_bias_correction_linear.weight)
+        self.linear.bias = nn.Parameter(self.linear.bias + self.plasticity_bias_linear.bias - self.plasticity_bias_correction_linear.bias)
+
 
         self.plasticity_bias.reset_parameters()
+        self.plasticity_bias_linear.reset_parameters()
+
         self.plasticity_bias_correction.load_state_dict(self.plasticity_bias.state_dict())
+        self.plasticity_bias_correction_linear.load_state_dict(self.plasticity_bias_linear.state_dict())
     
     def _change_grad(self, layer, requires_grad):
         for param in layer.parameters():
