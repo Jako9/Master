3fb5cadd6b268347edbf1d41f0add4f6618a37bb
Jako9 <cubecrafter.lpt@gmail.com>
2025-01-13 03:42:39 +0100

    Added smaller version of dqn-agent

--- network/__init__.py
+++ network/__init__.py
@@ -7,5 +7,7 @@ from .large_snn.rigid_snn import Rigid_SNN
 from .large_dnn.reset_network import Reset_Network
 from .large_snn.reset_snn import Reset_SNN
 from .large_snn.bucket_snn import Bucket_SNN
+from .small_dnn.small_ridig import Small_Rigid
+from .small_dnn.small_reset_network import Small_Reset_Network
 
-__all__ = ['Plastic', 'Plasticity_Injection', 'Layer_Norm', 'Reset_Head', 'Rigid', 'Rigid_SNN', 'Reset_Network', 'Reset_SNN', 'Bucket_SNN']
+__all__ = ['Plastic', 'Plasticity_Injection', 'Layer_Norm', 'Reset_Head', 'Rigid', 'Rigid_SNN', 'Reset_Network', 'Reset_SNN', 'Bucket_SNN', 'Small_Rigid', 'Small_Reset_Network']
--- network/base_network.py
+++ network/base_network.py
@@ -79,6 +79,30 @@ class Large_DNN(Plastic):
         x = self.body(x)
         return self.head(x)
     
+class Small_DNN(Plastic):
+    def __init__(self, env, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.conv2d_1 = nn.Conv2d(4, 64, 8, stride=2)
+        
+        self.relu = nn.ReLU()
+        self.flatten = nn.Flatten()
+
+        self.linear = nn.Linear(97344, 512)
+        self.head = nn.Linear(512, env.single_action_space.n)
+
+        self.body = nn.Sequential(
+            self.conv2d_1,
+            self.relu,
+            self.flatten,
+            self.linear,
+            self.relu
+        )
+
+    def _forward(self, x, global_step):
+        x = self.body(x)
+        return self.head(x)
+    
 import snntorch as snn
 import torch
 import numpy as np
