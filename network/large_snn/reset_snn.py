e4e2db992c3840b88e398178b907f4d676da4b9c
Jako9 <cubecrafter.lpt@gmail.com>
2025-02-19 16:02:38 +0100

    Fixed reset network for every architecture

--- network/large_snn/reset_snn.py
+++ network/large_snn/reset_snn.py
@@ -8,11 +8,11 @@ class Reset_SNN(Large_SNN):
 
         torch.save(self.state_dict(), f"{self.cache_folder}/initial_params.pth")
 
-def every_drift(self, num_drift):
-    loaded_params_dict = torch.load(f"{self.cache_folder}/initial_params.pth")
+    def every_drift(self, num_drift):
+        loaded_params_dict = torch.load(f"{self.cache_folder}/initial_params.pth")
 
-    if "_orig_mod." not in str(self.state_dict().keys()) and "_orig_mod." in str(loaded_params_dict.keys()):
-        adjusted_params_dict = {k.replace("_orig_mod.", ""): v for k, v in loaded_params_dict.items()}
-        loaded_params_dict = adjusted_params_dict
+        if "_orig_mod." not in str(self.state_dict().keys()) and "_orig_mod." in str(loaded_params_dict.keys()):
+            adjusted_params_dict = {k.replace("_orig_mod.", ""): v for k, v in loaded_params_dict.items()}
+            loaded_params_dict = adjusted_params_dict
 
-    self.load_state_dict(loaded_params_dict)
+        self.load_state_dict(loaded_params_dict)
